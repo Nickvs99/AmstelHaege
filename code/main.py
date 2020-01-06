906cc0024cb2bfebc192af8a18fec7827a83546e
hhiiddee1 <hidde.van.oijen@gmail.com>
2020-01-06 13:24:16 +0100

    main edit

--- code/main.py
+++ code/main.py
@@ -1,14 +1,24 @@
 """
-    
+
 """
 
 from area import area
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 def main():
-    
+
     new_area = area()
 
     print(new_area.area)
 
+    new_areanp = np.array(new_area)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(new_areanp)
+
+    fig.tight_layout()
+    plt.show()
 if __name__ == "__main__":
     main()

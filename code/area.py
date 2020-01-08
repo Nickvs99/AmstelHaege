542a4481c01023c93e7ff37121cb30d92d1d086f
Nickvs99 <vansantennick@gmail.com>
2020-01-08 11:25:59 +0100

    Does not work yet

--- code/area.py
+++ code/area.py
@@ -36,7 +36,7 @@ class area():
             for row in csv_reader:
                 bottom,left = row['bottom_left_xy'].split(",")
                 top,right  = row['top_right_xy'].split(",")
-                print(f"{bottom} , {left} - {top} , {right}")
+                # print(f"{bottom} , {left} - {top} , {right}")
 
                 for i in range(int(bottom),int(top)):
                     for j in range(int(left),int(right)):
@@ -51,16 +51,18 @@ class area():
         """ Places the houses randomly. """
 
         # Calculate the number of houses per type
-        one_person_house_count = int(0.6 * houses_count)
-        bungalow_count = int(0.25 * houses_count)
-        maison_count = int(0.15 * houses_count)
+        # one_person_house_count = int(0.6 * houses_count)
+        # bungalow_count = int(0.25 * houses_count)
+        # maison_count = int(0.15 * houses_count)
 
-        houses = self.create_houses(one_person_house_count, bungalow_count, maison_count)
+        one_person_house_count = 2
+        bungalow_count = 0
+        maison_count = 0
 
-        for house in houses:
+        self.houses = self.create_houses(one_person_house_count, bungalow_count, maison_count)
+
+        for house in self.houses:
             self.place_house(house)
-
-        print(houses)
             
     def create_houses(self, one_person_house_count, bungalow_count, maison_count):
         """ Creates a list with houses. """
@@ -83,6 +85,8 @@ class area():
         It picks a random x and y coordinate and then checks if there is room for the new house. 
         If it does not succeed, try new coordinates.
         """
+
+        # TODO not sure if this works when large houses gets placed first and then the smaller
 
         house_placed = False
         while_count = 0
@@ -128,7 +132,7 @@ class area():
                 y_temp = yCor + j
 
                 # If the mandatory free space falls outside of the grid, continue
-                if x_temp < 0 or y_temp < 0 or x_temp > self.width - 1 or y_temp > self.height - 1:
+                if not self.check_in_bound(x_temp, y_temp):
                     continue
 
                 # Check if the mandatory free space overlaps with a building
@@ -137,6 +141,115 @@ class area():
 
         return True
 
+    def calc_worth_area(self):
+        """ Calculates the worth of the area. """
+        
+        total_worth = 0
+        for  house in self.houses:
+            total_worth += self.calc_worth_house(house)
+
+        # total_worth += self.calc_worth_house(self.houses[1])
+
+        return total_worth
+
+    def calc_worth_house(self, house):
+        """ Calculates the worth of a house. """
+
+        # Get the free space surrounding the house
+        # TODO store these in the house object
+        bottom_left_cor = [house.x, house.y]
+        top_right_cor = [house.x + house.width - 1, house.y + house.height - 1]
+
+        while_count = 0
+        while True:
+
+            bottom_left_cor[0] -= 1
+            bottom_left_cor[1] -= 1
+            top_right_cor[0] += 1
+            top_right_cor[1] += 1
+            
+            elements = self.getBorder(bottom_left_cor, top_right_cor)
+            
+            # print()
+            # print(while_count)
+            # print(elements)
+
+            # if while_count > 53 and while_count < 56:
+            #     input()
+
+            # If a house is in the border break
+            if 2 in elements or 3 in elements or 4 in elements:
+                break
+
+            while_count += 1
+
+            if elements == []:
+                print("Whoops, something went wrong")
+                break
+
+
+        value = house.value + house.value * house.extra_value * (while_count - house.mandatory_free_space)
+
+        return value
+
+    def getBorder(self, bottom_left_cor, top_right_cor):
+        """ 
+        Returns all elements on the border from a rectangle spanned from
+        the bottom_left_cor to the top_right_cor.
+        """
+
+        # TODO refactor
+
+        elements = []
+
+        width = top_right_cor[0] - bottom_left_cor[0] + 1
+        height = top_right_cor[1] - bottom_left_cor[1] + 1
+
+        # Get the bottom and top border
+        for i in range(width):
+            
+            # Bottom 
+            x = bottom_left_cor[0] + i
+            y = bottom_left_cor[1]
+            if not self.check_in_bound(x, y):
+                continue
+            elements.append(self.area[y][x])
+
+            # Top
+            x = bottom_left_cor[0] + i
+            y = top_right_cor[1]
+            if not self.check_in_bound(x, y):
+                continue
+            elements.append(self.area[y][x])
+        
+        # Get the left and right border, excluding corners
+        for i in range(1, height):
+
+            # Left
+            x = bottom_left_cor[0]
+            y = bottom_left_cor[1] + i
+            if not self.check_in_bound(x, y):
+                continue
+            
+            elements.append(self.area[y][x])
+
+            # Right
+            x = top_right_cor[0]
+            y = bottom_left_cor[1] + i
+            if not self.check_in_bound(x, y):
+                continue           
+ 
+            elements.append(self.area[y][x])
+
+        return elements
+
+    def check_in_bound(self, x, y):
+        """ Checks if a given x and y coordinates fall within the bounds. """
+
+        if x < 0 or y < 0 or x >= self.width or y >= self.height:
+            return False
+        
+        return True
 
 class House():
 
@@ -146,23 +259,32 @@ class House():
         self.x = 0
         self.y = 0
 
+        # TODO presets or something, code doesnt look clean. Maybe three seperate object, 
+        # which inherit from this.
         if type_house == "bungalow":
             self.width = 11
             self.height = 7
             self.mandatory_free_space = 3
             self.state = 3
+            self.value = 399000
+            self.extra_value = 0.04
 
         elif type_house == "maison":
             self.width = 12
             self.height = 10
             self.state = 4
             self.mandatory_free_space = 6
+            self.value = 610000
+            self.extra_value = 0.03
 
         elif type_house == "one_person_home":
             self.width = 8
             self.height = 8
             self.state = 2
             self.mandatory_free_space = 2
+            self.value = 285000
+            self.extra_value = 0.06
+
         else:
             print("Invalid type_house")
     

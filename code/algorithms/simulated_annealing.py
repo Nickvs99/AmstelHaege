2fd32604c767163e99ab7467848d0cd395c4191c
hhiiddee1 <hidde.van.oijen@gmail.com>
2020-01-23 22:47:13 +0100

    simulated also done

--- code/algorithms/simulated_annealing.py
+++ code/algorithms/simulated_annealing.py
@@ -3,9 +3,14 @@ import random
 
 def simulated_annealing(area):
 
+    # Iteration
     for j in range(1,1000):
+
+        # Makes temperature
         T = 1000 - j
         worth = area.calc_worth_area()
+
+        # Tries to place house
         while True:
             k = int(random.random() * area.houses)
             house = area.structures["House"][k]
@@ -13,11 +18,17 @@ def simulated_annealing(area):
             old_y = house.bottom_left_cor[1]
             x = int(random.random() * (area.width - house.width + 1))
             y = int(random.random() * (area.height - house.height + 1))
+
+            # Places house when valid
             if area.check_valid(house, x, y):
                 move(area, house, x, y)
                 new_worth = area.calc_worth_area()
+
+                # Places house if worth more
                 if worth > new_worth:
                     chance = random.random() * 1000
+
+                    # Places house if worth less by chance
                     if T < chance:
                         move(area, house, old_x, old_y)
                 break
